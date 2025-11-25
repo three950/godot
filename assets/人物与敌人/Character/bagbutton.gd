3182beed9491211be19444f95d9ecbc69b45234c
莼朴烟花 <2869870457@qq.com>
2025-11-25 15:38:30 +0800

    使用add_child和remove_child来完成bag的加载和移出，很好。

--- assets/人物与敌人/Character/bagbutton.gd
+++ assets/人物与敌人/Character/bagbutton.gd
@@ -1,55 +1,44 @@
 extends Button
-
-# 背包相关
-var bag: Node = null  # 背包实例
+@onready var bag: bag_slot = $"../Bag"
+var is_open: bool = false
+var original_parent
 var character_node: Character = null
-
+var bag_scene: PackedScene = null
 func _ready() -> void:
-	var parent_node = get_parent()
-	if parent_node is Character:
-		character_node = parent_node
+	if bag:
+		print("有bag")
+		original_parent=bag.get_parent()
+	if original_parent is Character:
+		character_node = original_parent
 	else:
 		push_warning("【背包按钮】未找到 Character 节点")
-		return
-	bag = character_node.get_node_or_null("Bag")
-	if not bag:
-		push_warning("【背包按钮】未找到 Bag 节点")
 		return
 	pressed.connect(toggle_bag)
 	print("【背包按钮】初始化完成")
 # 切换背包显示状态
 func toggle_bag() -> void:
-	if bag.visible:
+	if is_open:
 		close_bag()
 	else:
 		open_bag()
 
 # 打开背包
 func open_bag() -> void:
+	text = "✖"
 	print("【背包按钮】打开背包")
 	if not bag or not is_instance_valid(bag):
 		push_warning("【背包按钮】无法打开背包：未找到背包节点")
 		return
 	
-	if bag.visible:return
-	bag.show()
-
-	_update_bag_button_state(true)
-	
+	original_parent.add_child(bag)
+	is_open = true
 	print("【背包按钮】背包显示完成")
 
 # 关闭背包
 func close_bag() -> void:
+	text = "🎒"
 	print("【背包按钮】关闭背包")
 	if bag and is_instance_valid(bag) and bag.visible:
-		bag.hide()
-		# 更新按钮状态
-		_update_bag_button_state(false)
-		print("【背包按钮】背包已关闭")
-
-# 更新背包按钮的状态
-func _update_bag_button_state(is_open: bool) -> void:
-	if is_open:
-		text = "✖"
-	else:
-		text = "🎒"
+		original_parent.remove_child(bag)
+	is_open = false
+	print("【背包按钮】背包已关闭")
