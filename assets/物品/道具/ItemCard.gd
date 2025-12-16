65183e053c49b3b4864aaa6ec30a319782b7c389
莼朴烟花 <2869870457@qq.com>
2025-12-17 00:05:22 +0800

    实现添加人物特性

--- assets/物品/道具/ItemCard.gd
+++ assets/物品/道具/ItemCard.gd
@@ -1,4 +1,4 @@
 extends "res://assets/物品/ThingsCard.gd"
 class_name ItemCard
 @export var card_scene:PackedScene = load("res://assets/物品/道具/item.tscn")
-@export var 特殊效果 :String
+@export var 添加特性 :String
