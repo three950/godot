d79b90ed61920e8cb04773d222cf538218850889
莼朴烟花 <2869870457@qq.com>
2025-12-21 21:27:06 +0800

    新增战斗panel

--- assets/card-states/card_fixed.gd
+++ assets/card-states/card_fixed.gd
@@ -8,6 +8,8 @@ func enter() -> void:
 	Events.stack_changed.emit(card)
 
 func _on_control_gui_input(event: InputEvent) -> void:
+	if card.battle.current_state==BattleState.Phase.BATTLE:
+		return
 	if event.is_action_pressed("LMB"):
 		if card.card_type == Card.cardType.architecture:
 			return
