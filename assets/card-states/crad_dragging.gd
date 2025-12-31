60c0c9867170fdcdb7066587e19ced2902463823
莼朴烟花 <2869870457@qq.com>
2025-12-31 20:42:22 +0800

    好吧，为了阴影，需要修改卡牌堆叠逻辑了，改成场景树

--- assets/card-states/crad_dragging.gd
+++ assets/card-states/crad_dragging.gd
@@ -1,10 +1,12 @@
 extends CardState
 
+
 func enter() -> void:#背包和商店的卡片可以用这个方法重新父化S1-03状态机
-	var card_layer := get_tree().get_first_node_in_group("Cards")
-	if card_layer:
-		card.reparent(card_layer)
-	card.z_index = 100
+	#var card_layer := get_tree().get_first_node_in_group("Cards")
+	#if card_layer:
+	#	card.reparent(card_layer)
+	#card.z_index = 100
+	pass
 
 func on_input(event: InputEvent) -> void:
 	card.global_position = card.get_global_mouse_position() - card.drag_offset
