900aec2c763d412e41f6761abcaa0bfe5057bf43
莼朴烟花 <2869870457@qq.com>
2026-05-08 14:26:08 +0800

    添加阴影

--- card_3d_scripts/states/card_3d_falling.gd
+++ card_3d_scripts/states/card_3d_falling.gd
@@ -12,9 +12,7 @@ func enter() -> void:
 
 	if card.children_card != null:
 		if card.children_card.card_state_machine and card.children_card.card_state_machine.current_state:
-			var child_state := card.children_card.card_state_machine.current_state
-			if child_state.has_signal("stop_follow_me"):
-				child_state.stop_follow_me.emit()
+			card.children_card.card_state_machine.current_state.stop_follow_me.emit()
 
 	if card.stack_state & Card3DState.STACK_STATE_STACKING:
 		transition_requested.emit(self, Card3DState.State.instack)
